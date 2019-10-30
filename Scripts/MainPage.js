7d4136864c61fbb93462b2b2cf201034aa85d193
Sameek Kundu <sameek@greenrain.io>
2019-10-30 11:04:17 +0530

    no message

--- Scripts/MainPage.js
+++ Scripts/MainPage.js
@@ -1,11 +1,64 @@
 ﻿
 
 const FeaturedProducts = [
-  { title: '<h6 class = "CardOverlayText2">Runtime</h6><h6  class = "CardOverlayText1">FBX Import</h6>', src: 'Images/ThumbNails/FBXImport.png' },
-  { title: '<h6  class = "CardOverlayText1" style = "margin-left:-20%; margin-top:8%;">FrameCapture</h6>', src: 'Images/ThumbNails/FrameCapture.png' },
-  { title: '<h6  class = "CardOverlayText2">Post Process</h6><h6 class = "CardOverlayText1">Blendables</h6>', src: 'Images/ThumbNails/PostProcessBlendables.png' },
-  { title: '<h6  class = "CardOverlayText2" style = "margin-left:-6%;">Minimap, Map and</h6><h6 class = "CardOverlayText1" style = "margin-left:-6%;"> Navigation</h6>', src: 'Images/ThumbNails/MapSystem.png' },
-  { title: '<h6  class = "CardOverlayText1" style = "margin-left:10px;">MySQL</h6><h6 class = "CardOverlayText2" style = "margin-left:-20px; margin-right:-20px;">Integration for Unreal</h6>', src: 'Images/ThumbNails/MySQL.png' },
+
+    {
+        title: '<h6 class = "CardOverlayText2">Runtime</h6><h6  class = "CardOverlayText1">FBX Import</h6>',
+        src: 'Images/ThumbNails/FBXImport.png',
+        OfficialSite: 'FBXImport.html',
+        DownloadLink: 'https://www.unrealengine.com/marketplace/en-US/slug/runtime-fbx-import-asynchronous',
+        DocumentationLink: 'Doc_FBXImport.html',
+        DiscordLink: 'https://discord.gg/hsejuTW'
+
+    },
+
+    {
+        title: '<h6  class = "CardOverlayText1" style = "margin-left:-10%; margin-top:4%;">FrameCapture</h6>',
+        src: 'Images/ThumbNails/FrameCapture.png',
+        OfficialSite: 'FBXImport.html',
+        DownloadLink: 'https://www.unrealengine.com/marketplace/en-US/slug/runtime-fbx-import-asynchronous',
+        DocumentationLink: 'Doc_FBXImport.html',
+        DiscordLink: 'https://discord.gg/hsejuTW'
+    },
+
+    {
+        title: '<h6  class = "CardOverlayText1" style = "margin-left:10px; margin-top:4%;">Microsoft SQL</h6><h6 class = "CardOverlayText2" style = "margin-left:-20px; margin-right:-20px;">Integration for Unreal</h6>',
+        src: 'Images/ThumbNails/MSSQL.png',
+        OfficialSite: 'FBXImport.html',
+        DownloadLink: 'https://www.unrealengine.com/marketplace/en-US/slug/runtime-fbx-import-asynchronous',
+        DocumentationLink: 'Doc_FBXImport.html',
+        DiscordLink: 'https://discord.gg/hsejuTW'
+    },
+
+
+    {
+
+        title: '<h6  class = "CardOverlayText2">Post Process</h6><h6 class = "CardOverlayText1">Blendables</h6>',
+        src: 'Images/ThumbNails/PostProcessBlendables.png',
+        OfficialSite: 'FBXImport.html',
+        DownloadLink: 'https://www.unrealengine.com/marketplace/en-US/slug/runtime-fbx-import-asynchronous',
+        DocumentationLink: 'Doc_FBXImport.html',
+        DiscordLink: 'https://discord.gg/hsejuTW'
+    },
+
+    {
+        title: '<h6  class = "CardOverlayText1" style = "margin-left:10px;  margin-top:4%;">MySQL</h6><h6 class = "CardOverlayText2" style = "margin-left:-20px; margin-right:-20px;">Integration for Unreal</h6>',
+        src: 'Images/ThumbNails/MySQL.png',
+        OfficialSite: 'FBXImport.html',
+        DownloadLink: 'https://www.unrealengine.com/marketplace/en-US/slug/runtime-fbx-import-asynchronous',
+        DocumentationLink: 'Doc_FBXImport.html',
+        DiscordLink: 'https://discord.gg/hsejuTW'
+    },
+
+    {
+        title: '<h6  class = "CardOverlayText2" style = "margin-left:-6%;">Minimap, Map and</h6><h6 class = "CardOverlayText1" style = "margin-left:-6%;"> Navigation</h6>',
+        src: 'Images/ThumbNails/MapSystem.png',
+        OfficialSite: 'FBXImport.html',
+        DownloadLink: 'https://www.unrealengine.com/marketplace/en-US/slug/runtime-fbx-import-asynchronous',
+        DocumentationLink: 'Doc_FBXImport.html',
+        DiscordLink: 'https://discord.gg/hsejuTW'
+    }
+
 ];
 
 
@@ -18,6 +71,11 @@ FeaturedProducts.forEach(Products => {
     const node = document.importNode(template.content, true);
     node.querySelector('.title').innerHTML = Products.title;
     node.getElementById("ThumbnailImage").src = Products.src;
+    node.getElementById("ImageCardLink").href = Products.OfficialSite;
+
+    node.getElementById("DownloadLink").href = Products.DownloadLink;
+    node.getElementById("DocumentationLink").href = Products.DocumentationLink;
+    node.getElementById("DiscordLink").href = Products.DiscordLink;
 
     parentdiv.appendChild(node);
 
--- Scripts/ProductTemplate.js
+++ Scripts/ProductTemplate.js
@@ -1,53 +1,64 @@
-﻿var tag = document.createElement('script');
-tag.id = 'iframe-demo';
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+﻿//var tag = document.createElement('script');
+//tag.id = 'iframe-demo';
+//tag.src = 'https://www.youtube.com/iframe_api';
+//var firstScriptTag = document.getElementsByTagName('script')[0];
+//firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('FeaturedVideoFrame', {
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-function onPlayerReady(event) {
-}
+//var player;
+//function onYouTubeIframeAPIReady() {
+//    player = new YT.Player('FeaturedVideoFrame', {
+//        events: {
+//            'onReady': onPlayerReady,
+//            'onStateChange': onPlayerStateChange
+//        }
+//    });
+//}
+//function onPlayerReady(event) {
+//}
 
-function changeBorderColor(playerStatus) {
-    var color;
-    if (playerStatus === -1) {
-        color = "#37474F"; // unstarted = gray
-    } else if (playerStatus === 0) {
-        color = "#FFFF00"; // ended = yellow
-    } else if (playerStatus === 1) {
-        color = "#FF6D00"; // playing = green
-    } else if (playerStatus === 2) {
-        color = "#DD2C00"; // paused = red
-    } else if (playerStatus === 3) {
-        color = "#AA00FF"; // buffering = purple
-    } else if (playerStatus === 5) {
-        color = "#FF6DOO"; // video cued = orange
-    }
-    if (color) {
-        document.getElementById('FeaturedVideoFrame').style.borderColor = color;
-    }
-}
+//function changeBorderColor(playerStatus) {
+//    var color;
+//    if (playerStatus === -1) {
+//        color = "#37474F"; // unstarted = gray
+//    } else if (playerStatus === 0) {
+//        color = "#FFFF00"; // ended = yellow
+//    } else if (playerStatus === 1) {
+//        color = "#FF6D00"; // playing = green
+//    } else if (playerStatus === 2) {
+//        color = "#DD2C00"; // paused = red
+//    } else if (playerStatus === 3) {
+//        color = "#AA00FF"; // buffering = purple
+//    } else if (playerStatus === 5) {
+//        color = "#FF6DOO"; // video cued = orange
+//    }
+//    if (color) {
+//        document.getElementById('FeaturedVideoFrame').style.borderColor = color;
+//    }
+//}
 
-function onPlayerStateChange(event) {
-    if (event.data === YT.Player.playerStatus.ENDED) {
-        $('#FeaturedVideoFrame').css('display', 'none');
-        $('#FeaturedVideoFrame').show();
-    }
+//function onPlayerStateChange(event) {
+//    if (event.data === YT.Player.playerStatus.ENDED) {
+//        $('#FeaturedVideoFrame').css('display', 'none');
+//        $('#FeaturedVideoFrame').show();
+//    }
 
-}
+//}
 
+var vid = document.getElementById("FeaturedVideo");
+vid.onended = function () {
+    vid.currentTime = 2; 
+    $('#FeaturedVideoThumb').show();
+    vid.style.display = 'none';
+}; 
 
-$('#PlayFeaturedVideo').on('click', function (ev) {
+$('#FeaturedVideoThumb').on('click', function (ev) {
     $(this).hide();
-    $("#FeaturedVideoFrame")[0].src += "&autoplay=1";
     ev.preventDefault();
-    $('#FeaturedVideoFrame').css('display', 'block');
+
+    vid.controls = true; 
+    vid.currentTime = 2; 
+    vid.style.display = 'block';
+
+    vid.play();
+
 });
